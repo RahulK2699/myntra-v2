b875cc569f82c6c3b27821b71ed9ec14ed923ced
Rahulkh2699 <rahul@scube.me>
2022-07-30 12:33:54 +0530

    user registration form completed

---
+++ server/database/migrations/1659161206895_users.ts
@@ -0,0 +1,28 @@
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+
+export default class UsersSchema extends BaseSchema {
+  protected tableName = "users";
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id").primary();
+      table.string("email", 255).notNullable();
+      table.string("password", 180).notNullable();
+      table.string("remember_me_token").nullable();
+
+      /**
+       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp("created_at", { useTz: true }).notNullable();
+      table.timestamp("updated_at", { useTz: true }).notNullable();
+      table.string("mob_no");
+      table.string("full_name");
+      table.string("gender");
+      table.string("alternate_no");
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
